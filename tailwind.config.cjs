b30641f69a1ddd1fc184e3a32c7337cc726894f6
akash9676 <akashnaidu545@gmail.com>
2022-11-13 13:12:42 +0530

    Final navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"button-colour": '#1a6c7a',
+				"button-hovercolour": "#153243",
+			},
+		},
 	},
 	plugins: [],
 };
