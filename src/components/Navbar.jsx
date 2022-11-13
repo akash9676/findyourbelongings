d62ed823319d571575974a2567bc0f436475174a
akash9676 <akashnaidu545@gmail.com>
2022-11-13 12:59:04 +0530

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ const Navbar = () =>{
     return(
         <navbar className="">
             <div className='flex p-2 items-center pr-16 pl-16'>
-                <div className='p-4'>
+                <div className='p-4 font-bold'>
                     Find Your Belongings
                 </div>
                 <div className='flex-1'></div>
                 <div className='flex pr-8'>
-                    <div className='font-bold text-xl p-4 pl-7 pr-7'>Home</div>
-                    <div className='font-bold text-xl p-4 pl-7 pr-7'>About</div>
-                    <div className='font-bold text-xl p-4 pl-7 pr-7'>Services</div>
-                    <div className='font-bold text-xl p-4 pl-7 pr-7'>Contact</div>
-                    <div className='font-bold text-xl p-4 pl-7 pr-7'>Contact Us</div>  
+                    <div className='font-medium text-xl p-4 pl-7 pr-7'>Home</div>
+                    <div className='font-medium text-xl p-4 pl-7 pr-7'>About</div>
+                    <div className='font-medium text-xl p-4 pl-7 pr-7'>Services</div>
+                    <div className='font-medium text-xl p-4 pl-7 pr-7'>Contact</div>
+                    <div className='font-medium text-xl p-4 pl-7 pr-7'>TAKE ACTION</div>  
                 </div>
             </div>
         </navbar>
