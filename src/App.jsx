e62e6232c7e98c67f96bdf031674fec05aa88d2f
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-13 14:07:14 +0530

    React-router-dom installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <>
-      <Navbar/>
-      <Footer/>
+      <Routes>
+        <Route path="/Navbar" element={<Navbar  />}/>
+        <Route path="/Footer" element={<Footer />}/>
+        <Route path="/About" element={<About  />}/>
+      </Routes>
     </>
-  )
+  );
 }
+
+
+
+
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import aboutBgPic from "../assets/aboutBgPic.jpg"
 
 const About = () => {
   return (
     <div>
-      
+      <div>
+        <img src={aboutBgPic}></img>
+      </div>
     </div>
   )
 }
