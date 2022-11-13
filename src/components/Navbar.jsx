6773e95e9d792e0ac436a62af03cba0f4892e5a2
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-14 00:18:38 +0530

    Fimenu icon added to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 
 //TODO: make navbar responsive "mobile view"
 
+import {FiMenu} from "react-icons/fi";
+
 const Navbar = () =>{
     return(
         <navbar className="">
             <div className="flex p-2 items-center pr-16 pl-16">
                 <div className="p-2 font-bold cursor-pointer">Find Your Belongings</div>
                 <div className="flex-1"></div>
-                <div className="flex pr-8 cursor-pointer">
+                <div className="hidden lg:flex pr-8 cursor-pointer">
                     <div className="font-medium text-xl p-2 pl-7 pr-7">
                         Home
                     </div>
@@ -22,6 +24,9 @@ const Navbar = () =>{
                     </div>
                     <div className="font-medium text-xl p-2 pl-7 pr-7 bg-button-colour hover:bg-button-hovercolour rounded-full">
                         TAKE ACTION
+                    </div>
+                    <div className="block items-center justify-center ml-5 lg:hidden">
+                        <FiMenu></FiMenu>
                     </div>
                 </div>
             </div>
