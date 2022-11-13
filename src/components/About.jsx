33cabdcdf72c7b04af9c09588f17e1398e0c1f9a
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-13 22:54:41 +0530

    About modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import aboutBgPic from "../assets/about-bg-pic.jpg";
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const About = () => {
@@ -9,37 +10,74 @@ const About = () => {
                 <div className="absolute flex justify-center items-center text-6xl text-black font-bold  w-full h-full">
                     About
                 </div>
-              <div className="absolute w-full"><Navbar/></div>
+                <div className="absolute w-full">
+                    <Navbar />
+                </div>
                 <img className="h-96 w-screen" src={aboutBgPic}></img>
             </div>
             <div className="flex mt-24 p-14 mx-96">
                 <div className="flex-1">
-                  <div className="text-2xl font-bold border-2 border-red-500">established in 1995</div>
+                    <div className="text-2xl font-bold">
+                        established in 1995
+                    </div>
                 </div>
                 <div className="flex-1">
-                  <div className="border-2 border-black">
-                      <div className="font-bold mb-8">
-                          Tell people about what you, your expertise and
-                          experience. Think about what you would want to see on
-                          this page if you were looking at an about page. This is
-                          your chance to tell about how you are qualified to serve
-                          them.
-                      </div>
-                      <div className="mb-8  text-gray-600">
-                          Nulla hendrerit metus et tincidunt tristique. Fusce
-                          molestie commodo mauris, vitae vestibulum tortor
-                          venenatis non. Pellentesque quis urna a magna bibendum
-                          volutpat a at nisl. Suspendisse finibus, tortor id
-                          gravida feugiat, nulla leo venenatis nisi, et aliquam
-                          quam ligula eget dui. Suspendisse lectus lorem, varius
-                          vel dapibus a, vestibulum a eros. Sed sed magna in neque
-                          commodo pretium.
-                      </div>
-                      <div className="mb-8  text-gray-600">We look forward to your stay.</div>
-                      <div className="mb-8 text-lg font-bold">John & Steven</div>
-                  </div>
+                    <div className="">
+                        <div className="font-bold mb-8">
+                            Tell people about what you, your expertise and
+                            experience. Think about what you would want to see
+                            on this page if you were looking at an about page.
+                            This is your chance to tell about how you are
+                            qualified to serve them.
+                        </div>
+                        <div className="mb-8  text-gray-600">
+                            Nulla hendrerit metus et tincidunt tristique. Fusce
+                            molestie commodo mauris, vitae vestibulum tortor
+                            venenatis non. Pellentesque quis urna a magna
+                            bibendum volutpat a at nisl. Suspendisse finibus,
+                            tortor id gravida feugiat, nulla leo venenatis nisi,
+                            et aliquam quam ligula eget dui. Suspendisse lectus
+                            lorem, varius vel dapibus a, vestibulum a eros. Sed
+                            sed magna in neque commodo pretium.
+                        </div>
+                        <div className="mb-8  text-gray-600">
+                            We look forward to your stay.
+                        </div>
+                        <div className="mb-8 text-lg font-bold">
+                            John & Steven
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div className="bg-amber-100">
+                <div className="flex mx-96">
+                    <div className="p-14 flex-1">
+                        <div className="text-2xl font-bold mb-5">Mission</div>
+                        <div>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Ut elit tellus, luctus nec ullamcorper mattis,
+                            pulvinar dapibus leo.
+                        </div>
+                    </div>
+                    <div className="p-14 flex-1">
+                        <div className="text-2xl font-bold mb-5">Vision</div>
+                        <div>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Ut elit tellus, luctus nec ullamcorper mattis,
+                            pulvinar dapibus leo.
+                        </div>
+                    </div>
+                    <div className="p-14 flex-1">
+                        <div className="text-2xl font-bold mb-5">Values</div>
+                        <div>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Luctus nec ullam corper</li>
+                            <li>Pulvinar dapibus leo</li>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div><Footer/></div>
         </div>
     );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="flex items-center justify-center w-screen border-t-2 border-gray-100 p-4">
-            <div className="text-gray-500 italic">
+        <div className="flex items-center justify-center w-screen border-t-2 border-gray-300 p-4">
+            <div className="text-gray-500 italic my-3">
                 Copyright © 2022 Mountain | Powered by Mountain
             </div>
         </div>
