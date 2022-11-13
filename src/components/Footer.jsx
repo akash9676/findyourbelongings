5692c2498df9e726f7f2d0bb03937b8ae1ca41d8
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-13 13:10:52 +0530

    Footer component created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex items-center justify-center w-screen border-t-2 border-gray-100 p-4'>
+      <div className='text-gray-500 italic'>Copyright © 2022 Mountain | Powered by Mountain</div>
+    </div>
+  )
+}
+
+export default Footer
